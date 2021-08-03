00078d8d7f8dde133ac952c483e18eb93ae3b476
Reginaldo Porfirio <porffiriocosta@gmail.com>
2021-08-03 20:48:26 -0300

    Criação componente Tabela

--- src/components/ValueDescriptor/ValueDescriptor.tsx
+++ src/components/ValueDescriptor/ValueDescriptor.tsx
@@ -1,29 +1,29 @@
+  
 import * as VD from './ValueDescriptor.styles'
 
-export interface ValueDescriptorProps{
-
-    description: string
-    value: number
-    color: 'primary' | 'default'
-    isCurrency?: boolean
-
+export interface ValueDescriptorProps {
+  description: string
+  value: number
+  color: 'primary' | 'default'
+  isCurrency?: boolean
 }
 
-export default function ValueDescriptor(props: ValueDescriptorProps){
+export default function ValueDescriptor (props: ValueDescriptorProps) {
+  const COLORS = {
+    primary: '#0099ff',
+    default: '#274060'
+  }
 
-    return <VD.Wrapper>
-
-    <span className="Description">{props.description}</span>
-
+  return <VD.Wrapper color={COLORS[props.color || 'default']}>
+    <span className="Description">{ props.description }:</span>
     <div>
-        {
-            props.isCurrency &&
+      {
+        props.isCurrency &&
           <span className="Currency">{'R$'}</span>
-        }
-
-    <span className="Value"></span>
-    {props.value.toLocaleString('pt-br')}
+      }
+      <span className="Value">
+        { props.value.toLocaleString('pt-br') }
+      </span>
     </div>
-
-    </VD.Wrapper>
+  </VD.Wrapper>
 }
